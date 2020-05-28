096e1ae795397d0e6e620376350da112cd8e61f4
neil <suvranilduttabiswas@gmail.com>
2020-05-28 21:48:56 +0530

    home screen and splash screen added

--- firebase_login/lib/main.dart
+++ firebase_login/lib/main.dart
@@ -1,12 +1,16 @@
+import 'package:firebase_login/home_screen.dart';
+import 'package:firebase_login/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:bloc/bloc.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'authentication_bloc/authentication_bloc.dart';
+import 'package:firebase_login/simple_bloc_delegate.dart';
+import 'package:firebase_login/authentication_bloc/authentication_bloc.dart';
 import 'package:firebase_login/user_repository.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  BlocSupervisor.delegate = SimpleBlocDelegate();
   final UserRepository userRepository = UserRepository();
   runApp(
     BlocProvider(
@@ -15,4 +19,29 @@ void main() {
       child: App(userRepository: userRepository),
     ),
   );
+}
+
+class App extends StatelessWidget {
+  final UserRepository _userRepository;
+
+  App({Key key, @required UserRepository userRepository})
+      : assert(userRepository != null),
+        _userRepository = userRepository,
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
+        builder: (context, state) {
+          if (state is Uninitialized) {
+            return SplashScreen();
+          }
+          if (state is Authenticated) {
+            return HomeScreen(name: state.displayName);
+          }
+        },
+      ),
+    );
+  }
 }
